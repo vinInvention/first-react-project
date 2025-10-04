d189a6745025f067da9bd1f52533abaee6de8341
vinInvention <vincentchinonyerem@gmail.com>
2025-10-04 06:38:29 +0100

    Remove node_modules from tracking

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (  
+        <nav className= "navbar">
+            <h1>The Dojo Blog</h1>
+            <ul className="links">
+                <a href="/">Home</a>
+                <a href="/create" >New Blog</a>
+            </ul>
+        </nav>
+
+    );
+}
+ 
+export default Navbar;
