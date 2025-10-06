eb5891f36afcf523421f0503c2d8363c614edabe
vinInvention <vincentchinonyerem@gmail.com>
2025-10-06 07:56:24 +0100

    Adding the projects again to the repository

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    const handleClick = () =>{
+        console.log('You just clicked me!')
+    }
+    return (  
+        <section className= "home">
+	        <h2>Hello World</h2>
+        </section>
+        
+    );
+}
+ 
+export default Home;
